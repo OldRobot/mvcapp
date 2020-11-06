56430e4458fde9436ed28ddfa1e9dfef8b85e64b
Paul Smith <paulsmith@Pauls-MacBook-Pro.local>
2020-11-06 16:33:45 +0000

    editing posts started

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -85,4 +85,64 @@
             ];
             $this->view('posts/show', $data);
         }
+
+    
+
+        public function edit($id){
+            //check to see if there has been a post
+            if($_SERVER['REQUEST_METHOD']=='POST'){
+              
+                                //if data is submitted , sanatise the post
+                $_POST = filter_input_array(INPUT_POST,FILTER_SANITIZE_STRING);
+                
+                $data =[
+                    'title' => trim($_POST['title']),
+                    'body' => trim($_POST['body']),
+                    'user_id' => $_SESSION['user_id'],
+                    'title_err' => '',
+                    'body_err' => ''
+                ];
+
+                //validate 
+                if(empty($data['title'])){
+                    $data['title_err'] = 'Please enter a title';
+                }
+                if(empty($data['body'])){
+                    $data['body_err'] = 'Please enter body text';
+                }
+                
+                //make sure there are no errors
+                if(empty($data['title_err']) && empty($data['body_err'])){
+                    if($this->postModel->updatePost($data)){
+                        flash('post_message','Post Updated');
+                        redirect('posts');
+                    }else{
+                        die('Something went wrong');
+                    }
+
+                }else{
+                    //load the view with the errors
+                    $this->view('posts/edit',$data);
+                }
+
+             
+
+            }else{
+                //get the existing post
+                $post = $this->postModel->getPostById($id);
+                //check for owner - if the logged in user
+                //is the owner of the post
+                if($post->user_id != $_SESSION['user_id']){
+                    //redirect them
+                    redirect('posts');
+                }
+                $data =[
+                    'id' => $id,
+                    'title' => $post->title,
+                    'body' => $post->body
+                ];
+    
+                $this->view('posts/edit',$data);
+            }
+        }
     }
--- app/views/posts/edit.php
+++ app/views/posts/edit.php
@@ -2,9 +2,9 @@
         <a href="<?php echo URLROOT;?>/posts" class="btn btn-light"><i class="fa fa-backward"></i> Back</a>
 
         <div class="card card-body bg-light mt-5">
-            <h2>Add post</h2>
+            <h2>Edit post</h2>
             <p>Create a post with this form</p>
-            <form action="<?php echo URLROOT;?>/posts/add" method='post'>
+            <form action="<?php echo URLROOT;?>/posts/edit/<?php echo $data['id']; ?>" method='post'>
            
             <div class="form-group">
                 <label for="title">Title: <sup>*</sup></label>
@@ -17,7 +17,7 @@
             <div class="form-group">
                 <label for="body">Body: <sup>*</sup></label>
                 <textarea  name="body" class="form-control form-control-lg<?php echo (!empty($data['body_err'])
-                 ) ? 'is-invalid' : ''; ?>"><?php echo $data['body'];?></textarea>
+                ) ? 'is-invalid' : ''; ?>"><?php echo $data['body'];?></textarea>
                 <span class="invalid-feedback"><?php echo $data['body_err'];?></span>
             </div>
             <input type="submit" class="btn btn-success" value="Submit">
